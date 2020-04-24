b7e5ade92e38c00da9b3f6be6a8def070495c185
Homarv <pierratmarvin3@gmail.com>
2020-04-24 10:34:42 +0200

    c'est de la merde

--- app.rb
+++ app.rb
@@ -1,28 +1,30 @@
 require 'bundler'
 Bundler.require
 
-require_relative 'lib/game'
 require_relative 'lib/player'
-
-player1 = Player.new("Sacha")
-player2 = Player.new("Régis")
-
-
-while player1.life_points > 0 && player2.life_points >0 
-    puts "Voici l'état de chaque joueur :"
-    player1.show_state
-    player2.show_state 
-    puts ""
-
-    puts "Passons à la phase d'attaque :"
-    player2.attacks(player1)
- if player1.life_points <=0
-    break
-    end 
-    player1.attacks(player2)
-    puts""
-    
-end
+require_relative 'Board'
+require_relative 'BoardCase'
+require_relative 'show'
 
 
+puts "Joueur 1 choisi ton pseudo?"
+player_name = gets.chomp
+
+#importation des paramètre joueur 
+player1 = Player.new(player_name , "X")
+
+puts "Joueur 2 choisi ton pseudo ?"
+player_name2 = gets.chomp
+
+#importation des paramètre joueur 
+player2 = Player.new(player_name2 , "Y")
+
+
+#importation d'un board
+board = Board.new
+p board
+
+binding.pry
+
+p "end of program"
 
--- lib/player.rb
+++ lib/player.rb
@@ -1,93 +1,11 @@
 require "pry"
 
 class Player
-    attr_accessor :name, :life_points
+    attr_accessor :name, :symbol
   
-    def initialize(name)
-      @life_points =10
+    def initialize(name, symbol )
+      @symbol = symbol
       @name= name
     end
 
-    def show_state
-      puts "#{@name} à #{@life_points} de vie"
-    end
-
-    def gets_damage (damage)
-      @life_points -= damage
-      if @life_points <= 0 
-      puts "le joueur #{@name} a été tué !"
-      else 
-      end
-      return
-    end
-
-    def attacks (player)
-      puts "le joueur #{@name} attaque le joueur #{player.name}"
-      damage = player.compute_damage
-      puts "il lui inflige #{damage} points de dommages"
-      player.gets_damage(damage)
-    end
-
-    def compute_damage
-        return rand(1..6)
-    end
-
-end
- 
-
-class HumanPlayer < Player
-   attr_accessor :weapon_level
-
-   def initialize(name)
-   @weapon_level = 1
-   super(name) 
-   @life_points = 100
-  end
-
-  def show_state
-    puts "#{@name} à #{@life_points} de vie et une arme de niveau #{@weapon_level}"
-  end
-
-  def compute_damage
-    rand(1..6) * @weapon_level
-  end
-
-  def search_weapon
-    weapon_dice = rand(1..6)
-    "Tu as trouvé une arme de niveau #{weapon_dice}"
-    if weapon_dice> weapon_level
-      @weapon_level=weapon_dice
-      puts "Youhou ! elle est meilleure que ton arme actuelle : tu la prends."
-    else 
-      puts "M@*#$... elle n'est pas mieux que ton arme actuelle..."
-    end
-  end
-
-  def search_health_pack
-
-    health_dice = rand(1..6)
-    
-    if health_dice == 1
-      puts "Tu n'as rien trouvé... "
-    elsif health_dice>=2 && health_dice<=5
-      n=50
-      while @life_points <100 && n>0 
-        n-1
-        @life_points +=1
-      end
-      puts "Bravo, tu as trouvé un pack de +50 points de vie, tu as maintenant #{@life_points} Pv!"
-    else
-      n=80
-      while @life_points <100 && n>0 
-        n-1
-        @life_points +=1
-      puts "Waow, tu as trouvé un pack de +80 points de vie #{@life_points} Pv !"
-      end
-    end
-  end
-
-end
-
-
-binding.pry
-p "end of program"
+end 
